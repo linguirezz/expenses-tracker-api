a574ef11c943b6e96c6ad4d9136cddc94dadaeb1
ALNI <alinuriman000@gmail.com>
2024-09-14 21:29:54 +0700

    mkmk

--- utils/jwt_token.js
+++ utils/jwt_token.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config();
 
 console.log('SECRET:', process.env.SECRET); // This should print your secret key
 
