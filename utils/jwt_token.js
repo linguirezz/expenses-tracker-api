34fbcb69d145a57c0bc832751900d4aff113727a
ALNI <alinuriman000@gmail.com>
2024-09-14 21:25:17 +0700

    recode the jwt generator

--- utils/jwt_token.js
+++ utils/jwt_token.js
@@ -1,16 +1,16 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-const jwt = require('jsonwebtoken')
-// JWT GENERATOR
-// ? how it can be secure ? 
+
+console.log('SECRET:', process.env.SECRET); // This should print your secret key
+
+const jwt = require('jsonwebtoken');
 
 function jwtGenerator(user_id) {
     const payload = {
-        id :user_id
-    }
-    const token = jwt.sign(payload,process.env.SECRET,{expiresIn:'1h'})
-    return token
+        id: user_id
+    };
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1h' });
+    return token;
 }
-module.exports= jwtGenerator
 
-
+module.exports = jwtGenerator;
