c30550a4413e5873f1ab13f58e9e98eaa0188394
ALNI <alinuriman000@gmail.com>
2024-09-14 20:39:22 +0700

    database function

--- database/pool.js
+++ database/pool.js
@@ -7,28 +7,6 @@ const pool = new Pool({
         connectionString: process.env.POSTGRES_URL
       
 })
-async function createTable() {
-  const client = await pool.connect();
-  try {
-    const createTableQuery = `
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(50) NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password TEXT NOT NULL
-      );
-      INSERT INTO users (username, email, password) VALUES ('admin', 'admin000@gmail.com', 'admin123'); 
-    `;
-    await client.query(createTableQuery);
-    
-    console.log('Table created successfully');
-  } catch (err) {
-    console.error('Error creating table', err);
-  } finally {
-    client.release();
-  }
-}
 
-createTable();
 
 module.exports=pool
