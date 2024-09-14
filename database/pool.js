efb8d1644a0e5a8575e5a7e1d1ba0d5f12e218f7
ALNI <alinuriman000@gmail.com>
2024-09-14 10:10:18 +0700

    host the database

--- database/pool.js
+++ database/pool.js
@@ -4,12 +4,29 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const {Pool} = require('pg')
 console.log(process.env.HOST)
 const pool = new Pool({
- host : process.env.HOST,
- user: process.env.USER, 
- database:process.env.DATABASE,
- password: process.env.PASSWORD,
- max:1000,
- idleTimeoutMillis:30000,
- connectionTimeoutMillis:3000,
+        connectionString: process.env.POSTGRES_URL
+      
 })
+async function createTable() {
+  const client = await pool.connect();
+  try {
+    const createTableQuery = `
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      );
+    `;
+    await client.query(createTableQuery);
+    console.log('Table created successfully');
+  } catch (err) {
+    console.error('Error creating table', err);
+  } finally {
+    client.release();
+  }
+}
+
+createTable();
+
 module.exports=pool
